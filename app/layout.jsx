026a5c7cadef33dfe8111caaa5d97a56e5022392
nex7-7 <snehil.sinha784@gmail.com>
2025-12-25 18:20:59 +0530

    Commit before Tailwind v4 upgrade

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
       <body
         className={cn(
